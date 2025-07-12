2e5d29cefcbc785297d179d16b47949b04444ad7
mAliz007 <m.ali.zafar.005@gmail.com>
2025-07-12 23:05:39 +0500

    //Day one progress 37min

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import SignupForm from './_auth/forms/SignupForm';
 import Home from './_root/pages/Home';
 import { RootLayout } from './_root/RootLayout';
 import SigninForm from './_auth/forms/SigninForm';
+import AuthLayout from './_auth/AuthLayout';
 
 function App() {
   return (
     <main className="flex h-screen">
       <Routes>
-        <Route element={<RootLayout />}>
-        <Route path="/sign-in" element={<SigninForm />}/>
-        <Route path="/sign-up" element={<SignupForm />}/>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
         </Route>
         <Route element={<RootLayout />}>
-        <Route index element={<Home />}/>
+          <Route index element={<Home />} />
         </Route>
       </Routes>
 
--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,25 @@
 
+import { Outlet, Navigate } from "react-router-dom"
 const AuthLayout = () => {
+  const isAuthenticated = false; // Replace with actual authentication logic
+
   return (
-    <div>AuthLayout</div>
+    <>
+    {
+      isAuthenticated ? (
+        <Navigate to="/" />):(
+          <>
+          <section className=  "flex flex-1 items-center justify-center flex-col py-10">
+
+            <Outlet />
+          </section>
+
+          <img src="/assets/images/side-img.svg" alt="logo" className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"/>
+
+          </>
+        )
+    }
+    </>
   )
 }
 
